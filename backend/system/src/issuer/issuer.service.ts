e2b8c5553e061417de01cccb26c3ad231addf4c1
ksiyeon27 <siyeonk27@gmail.com>
2024-04-28 20:43:08 +0900

    add jwtService: create_vc_jwt

--- backend/system/src/issuer/issuer.service.ts
+++ backend/system/src/issuer/issuer.service.ts
@@ -1,12 +1,15 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { RequestCareerVcDTO } from './dto/request-vc.dto';
+import { RequestCareerVcDTO } from './dto/request-career-vc.dto';
 import { Career } from './entities/career.entity';
-import { CreateVcDTO } from './dto/create-vc.dto';
-import { VC } from './dto/vc.dto';
+import { Claims } from './dto/claims.dto';
 import { Player } from './entities/player.entity';
+import { JwtService } from 'src/jwt/jwt.service';
 
 @Injectable()
 export class IssuerService {
+    constructor(readonly jwtService: JwtService) {}
+
+
     private careers: Career[] = []; //DB table
 
     private issuer = new Player({
@@ -20,6 +23,7 @@ export class IssuerService {
             ...{
                 department: "개발부서",
                 position: "대리",
+                salary: 50000, 
                 join: "20221222",
                 leave: "20241222" 
             }
@@ -30,54 +34,45 @@ export class IssuerService {
             ...{
                 department: "마케팅부서",
                 position: "팀장",
+                salary: 70000,
                 join: "20181222",
                 leave: "20231222" 
             }
         });
     }
 
-    request_vc(vc_request_data: RequestCareerVcDTO): VC{
+    request_vc(career_vc_request_data: RequestCareerVcDTO): Promise<string>{
 
         // 1. 홀더 검증 : DID resolver API 호출해서 did docs 얻어오고, 난수(vc_request_data.nonce) 복호화 시도
         // 제이가 만든 리졸버 이용
         // GET /did/{did}
         
         // issuer DB 에서 career 가져오기
-        const career = this.careers.find((career) => career.id === vc_request_data.holder_did)
+        const career = this.careers.find((career) => career.id === career_vc_request_data.holder_did)
         if (!career) {
             throw new NotFoundException(); //안에 message 가능
           }
 
-        // 2. VC 생성 -> this._create_vc 호출
-            // 이 안에서는 sd-jwt 이용해서 VC 생성
-        const create_vc_data = new CreateVcDTO({
-            holder_did: vc_request_data.holder_did,
+        // 2. VC 생성 
+        const vc_claims = this._create_vc_claims(career)
+
+        const new_vc_did = "VC 의 id 인 did"  // POST /did/{did}
+        const new_vc = this.jwtService.create_vc_jwt(vc_claims, new_vc_did, this.issuer.id, career_vc_request_data.holder_did)
+        
+        return new_vc
+    }
+
+    _create_vc_claims(career: Career): Claims{
+        //VC 생성 (추후 sd-jwt : issuer 의 pirvate_key 이용)
+        // POST /did/{did}?
+        const claims = new Claims({
             department: career.department,
             position: career.position,
             join: career.join,
             leave: career.leave
         });
-        console.log(create_vc_data)
-        const new_vc = this._create_vc(create_vc_data)
 
-        return new_vc
+        return claims;
     }
 
-    _create_vc(create_vc_data: CreateVcDTO): VC{
-        //VC 생성 (추후 sd-jwt : issuer 의 pirvate_key 이용)
-        const new_vc_did = "VC 의 id 인 did"  // POST /did/{did}?
-        const vc = new VC({
-            id: new_vc_did,
-            issuer: this.issuer.id,
-            subject: create_vc_data.holder_did,
-            department: create_vc_data.department,
-            position: create_vc_data.position,
-            join: create_vc_data.join,
-            leave: create_vc_data.leave
-        });
-
-        console.log(vc);
-
-        return vc;
-    }
 }
